49819a3336e414f292edfd3ad476290732e86de3
Roman Belinsky <romanbelinsky99@gmail.com>
2023-11-06 04:35:58 +0600

    Move kotlin code to kotlin package

--- src/main/kotlin/com/rombe/papermc/plguinname/PluginTemplate.kt
+++ src/main/kotlin/com/rombe/papermc/plguinname/PluginTemplate.kt
@@ -3,7 +3,7 @@ package com.rombe.papermc.plguinname
 import org.bukkit.plugin.java.JavaPlugin
 
 class PluginTemplate : JavaPlugin() {
-    override fun onEnable() = logger.info("Plugin template enabled ${listOf(1, 2, 3).find { it == 5 } ?: -1}")
+    override fun onEnable() = logger.info("Plugin template enabled")
 
     override fun onDisable() = logger.info("Plugin template disabled")
 }
